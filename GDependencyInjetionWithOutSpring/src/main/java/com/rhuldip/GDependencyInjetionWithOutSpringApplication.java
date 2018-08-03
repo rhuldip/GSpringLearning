e11b6a3d1b2fb375d0210735ba15a8d8e98ab9cb
Rahul Deep <rhuldip@gmail.com>
2018-08-03 21:58:00 +0530

    Empty Spring boot project

---
+++ GDependencyInjetionWithOutSpring/src/main/java/com/rhuldip/GDependencyInjetionWithOutSpringApplication.java
@@ -0,0 +1,12 @@
+package com.rhuldip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GDependencyInjetionWithOutSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GDependencyInjetionWithOutSpringApplication.class, args);
+	}
+}
